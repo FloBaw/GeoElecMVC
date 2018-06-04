e2a356b5883622088a3cf025a6db004bf730ddca
OlivierHouben <olivierhouben2000@outlook.com>
2018-06-04 13:50:20 +0200

    creation classe generatorfrme

---
+++ GeoElecMVC/Models/GeneratorFrame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoElecMVC.Models
+{
+    public class GeneratorFrame
+    {
+        [Key]
+        public long log_id { get; set; }
+
+        [Required]
+        public string log_timestamp { get; set; }
+
+        [Required]
+        public DateTime log_date { get; set; }
+
+        [Required]
+        public string generator_id { get; set; }
+
+        [Required]
+        public double nrj_tot { get; set; }
+
+        public double power_1 { get; set; }
+
+        public double power_2 { get; set; }
+
+        public double power_3 { get; set; }
+
+        public double current_1 { get; set; }
+
+        public double current_2 { get; set; }
+
+        public double current_3 { get; set; }
+
+        public double voltage { get; set; }
+
+        public bool relay_1 { get; set; }
+
+        public bool relay_2 { get; set; }
+
+        public bool magnetic { get; set; }
+
+        public double latitude { get; set; }
+
+        public double longitude { get; set; }
+
+
+
+
+        public bool payment { get; set; }
+    }
+}
+}
---
+++ GeoElecMVC/Repository/GeneratorRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoElecMVC.Services
+{
+    public class GeneratorRepository: IRepository<Generator>
+    {
+    }
+}
