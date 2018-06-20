c23d14142837f92a073c3fbf9158c671eaf20d78
Baw <florian.bawin@hotmail.com>
2018-06-20 14:58:49 +0200

    GeoElec Contact

--- GeoElecMVC/Models/ContactViewModel.cs
+++ GeoElecMVC/Models/ContactViewModel.cs
@@ -10,17 +10,17 @@ namespace GeoElecMVC.Models
     public class ContactViewModel
     {
         [StringLength(100)]
-        [Required(ErrorMessage = "The name field is required")]
+        [Required(ErrorMessage = "Name field is required")]
         [Display(Name = "Name")]
         public string Name { get; set; }
 
-        [EmailAddress(ErrorMessage = "The Email field is wrong")]
-        [Required(ErrorMessage = "The Email field is required")]
+        [EmailAddress(ErrorMessage = "Email field is wrong")]
+        [Required(ErrorMessage = "Email field is required")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [StringLength(100)]
-        [Required(ErrorMessage = "The subject field is required")]
+        [Required(ErrorMessage = "Subject field is required")]
         [Display(Name = "Subject")]
         public string Subject { get; set; }
 
